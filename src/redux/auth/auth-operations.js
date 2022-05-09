58d11e549ae7b7e1f7d05afe062f28f34abe5303
Dmitriy Dudnik <84873427+Wed88@users.noreply.github.com>
2022-05-09 23:05:05 +0300

    добавляет регистрацию юзера

---
+++ src/redux/auth/auth-operations.js
@@ -0,0 +1,20 @@
+import axios from "axios";
+import { createAsyncThunk } from "@reduxjs/toolkit";
+
+axios.defaults.baseURL = 'https://connections-api.herokuapp.com'
+
+const register = createAsyncThunk('auth/register', async credentials => {
+  try {
+    const { data } = await axios.post('/users/signup', credentials);
+    // token.set(data.token);
+    return data;
+  } catch (error) {
+   console.log(error.message)
+  }
+});
+
+const operations = {
+    register
+}
+
+export default operations
