a38f9e2243fa6a75c2c2bb78ef7f16cbcc3dbf46
Dmitriy Dudnik <84873427+Wed88@users.noreply.github.com>
2022-05-14 20:44:55 +0300

    Update Filter.jsx

--- src/components/Filter/Filter.jsx
+++ src/components/Filter/Filter.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { FilterContainer, Label, Input } from './Filter.styled';
+import { FilterContainer, Label } from './Filter.styled';
 import TextField from '@mui/material/TextField';
 
 const Filter = ({value, changeFilter}) => {
